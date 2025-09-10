cb4d86032405d57ab16d7af940da903bfd3c59ab
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-09-10 14:09:34 +0800

    HF reco

---
+++ refcode/JpsiReco_XD/filelist/create.sh
@@ -0,0 +1,6 @@
+#CreateDstList.pl DST_CALO_run2pp --tag ana468_2024p012_v001 --list run.list
+#CreateDstList.pl DST_CALO --tag ana468_2024p012_v001 --list run.list
+CreateDstList.pl DST_CALOFITTING --tag new_newcdbtag_v006 --list run.list
+CreateDstList.pl DST_TRKR_CLUSTER_run2pp --tag ana494_2024p021_v001 --list run.list
+#CreateDstList.pl DST_TRKR_SEED_run2pp --tag ana494_2024p021_v001 --list run.list
+CreateDstList.pl DST_TRKR_TRACKS_run2pp --tag ana495_2024p021_v001 --list run.list
