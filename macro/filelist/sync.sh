8bd9b0377a8e230a8ac2d81027fa82681ddf1e12
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-09-14 21:19:14 +0800

    Update sync.sh

--- macro/filelist/sync.sh
+++ macro/filelist/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-runs=(53046) #test run
-# runs=(53046 53079 53081 53195 53494 53513 53517 53530 53531 53532 53571 53578 53580 53581 53590 53630 53631 53632 53652 53687 53716 53742 53744 53756) #green runs, both good Tracking and calo runs
+# runs=(53046) #test run
+runs=(53046 53079 53081 53195 53494 53513 53517 53530 53531 53532 53571 53578 53580 53581 53590 53630 53631 53632 53652 53687 53716 53742 53744 53756) #green runs, both good Tracking and calo runs
 #runs=(53018 53194 53196 53197 53586 53587 53741 53743 53783 53871 53876 53877) #yellow runs
 
 # set the ratio of number of calo files over number of trkr files
@@ -47,8 +47,6 @@ do
         # run number: 00053741
         # segment number: 00001
 
-        echo "[DEBUG] trkr_tracks_line=$trkr_tracks_line -> segment=$trkr_tracks_segment"
-
         # store in synchronization map, key is segment
         trkr_tracks_map["$trkr_tracks_segment"]="$trkr_tracks_line"
     done < "$input_trkr_tracks_file"
@@ -63,8 +61,6 @@ do
         calo_run=$(echo "$calo_line" | cut -d'-' -f2)
         calo_segment=$(echo "$calo_line" | cut -d'-' -f3 | cut -d'.' -f1)
 
-        echo "[DEBUG] calo_line=$calo_line -> segment=$calo_segment"
-
         # store in synchronization map, key is segment
         calo_map["$calo_segment"]="$calo_line"
     done < "$input_calo_file"
@@ -72,8 +68,8 @@ do
     # loop over all tracker segments, write out only when calo can also match with increased order
     for trkr_segment in $(printf "%s\n" "${!trkr_tracks_map[@]}" | sort -n); do
         matched_calo_segment=$(printf "%05d" $((10#$trkr_segment / ratio_nCalo_over_nTrkr)))
-        # 调试输出可保留观察次序
-        echo "[DEBUG-ORDERED] trkr_segment=${trkr_segment} -> calo_segment=${matched_calo_segment}"
+        # # check output the matching results
+        # echo "[DEBUG-ORDERED] trkr_segment=${trkr_segment} -> calo_segment=${matched_calo_segment}"
         if [[ -n "${calo_map[$matched_calo_segment]}" ]]; then
             echo "$trkr_segment" "$matched_calo_segment" >> "$output_trkr_tracks_cluster_calo_file"
         fi
