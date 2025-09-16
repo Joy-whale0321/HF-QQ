8db9ab610f456591a6cceb678501c16dfcf219fc
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-09-16 21:13:35 +0800

    Update create.sh

--- macro/filelist/create.sh
+++ macro/filelist/create.sh
@@ -3,7 +3,7 @@
 # CreateDstList.pl DST_CALOFITTING --tag new_newcdbtag_v006 --list run.list
 # CreateDstList.pl DST_TRKR_CLUSTER_run2pp --tag ana494_2024p021_v001 --list run.list
 #CreateDstList.pl DST_TRKR_SEED_run2pp --tag ana494_2024p021_v001 --list run.list
-CreateDstList.pl DST_TRKR_TRACKS_run2pp --tag ana495_2024p021_v001 --list run.list
+# CreateDstList.pl DST_TRKR_TRACKS_run2pp --tag ana495_2024p021_v001 --list run.list
 # CreateDstList.pl DST_TRKR_TRACKS_run2pp --tag ana506_2024p023_v001 --list run.list
 
 # CreateDstList.pl DST_CALO --tag ana509_2024p022_v001 --run runnumber
