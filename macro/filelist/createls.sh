21affa12f0f0c923427e3f8d4d62d34e784f25d6
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-09-16 21:09:01 +0800

    Update createls.sh

--- macro/filelist/createls.sh
+++ macro/filelist/createls.sh
@@ -27,11 +27,10 @@ while read run || [[ -n "$run" ]]; do
     directory="/sphenix/lustre01/sphnxpro/production/${runspecies}/${runtype}/${tagversion}/${dsttype}/run_${formatted_lower}_${formatted_upper}/dst/"
 
     fileHeader=${dsttype}_${runspecies}_${tagversion}-${run}-
-    # echo "printing ${directory}/${fileHeader}*.root -> ${outfile}"
+    echo "printing ${directory}/${fileHeader}*.root -> ${outfile}"
     cd ${directory}
     ls ${fileHeader}*.root > ${outfile}
 done < run.list
-
 
 cd ${localpath}
 # 逐行读取 run.list
