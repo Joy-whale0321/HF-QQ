c34fb17f1fb42846092f4d0b01771eec0bc1882f
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-09-30 12:47:03 +0800

    Update Fun4All_PhotonConv_Reco.C

--- macro/Fun4All_PhotonConv_Reco.C
+++ macro/Fun4All_PhotonConv_Reco.C
@@ -438,9 +438,9 @@ void KFPReco(std::string module_name = "KFPReco", std::string decaydescriptor = 
     kfparticle->set_emcal_radius_user(new_cemc_rad);
     //narrow window
     kfparticle->set_dphi_cut_low(-0.02); //rad
-    kfparticle->set_dphi_cut_high(0.09); //rad
-    kfparticle->set_dz_cut_low(-10); //cm
-    kfparticle->set_dz_cut_high(10); //cm
+    kfparticle->set_dphi_cut_high(0.02); //rad
+    kfparticle->set_dz_cut_low(-2); //cm
+    kfparticle->set_dz_cut_high(2); //cm
     //loose window
     /*
     kfparticle->set_dphi_cut_low(-0.2); //rad
