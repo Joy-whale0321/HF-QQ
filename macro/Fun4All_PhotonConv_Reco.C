e1fc6749fd065cb5e6e21b2360bd3ba5df51d30f
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-10-05 19:22:05 +0800

    Update Fun4All_PhotonConv_Reco.C

--- macro/Fun4All_PhotonConv_Reco.C
+++ macro/Fun4All_PhotonConv_Reco.C
@@ -221,22 +221,22 @@ void Fun4All_PhotonConv_Reco(
 
     TpcReadoutInit( runnumber );
     // these lines show how to override the drift velocity and time offset values set in TpcReadoutInit
-    // G4TPC::tpc_drift_velocity_reco = 0.0073844; // cm/ns
-    // TpcClusterZCrossingCorrection::_vdrift = G4TPC::tpc_drift_velocity_reco;
-    // G4TPC::tpc_tzero_reco = -5*50;  // ns
+    G4TPC::tpc_drift_velocity_reco = 0.0073844; // cm/ns
+    TpcClusterZCrossingCorrection::_vdrift = G4TPC::tpc_drift_velocity_reco;
+    G4TPC::tpc_tzero_reco = -5*50;  // ns
 
-    // G4TPC::ENABLE_MODULE_EDGE_CORRECTIONS = true;
+    G4TPC::ENABLE_MODULE_EDGE_CORRECTIONS = true;
 
-    // // to turn on the default static corrections, enable the two lines below
-    // G4TPC::ENABLE_STATIC_CORRECTIONS = true;
-    // G4TPC::USE_PHI_AS_RAD_STATIC_CORRECTIONS = false;
+    // to turn on the default static corrections, enable the two lines below
+    G4TPC::ENABLE_STATIC_CORRECTIONS = true;
+    G4TPC::USE_PHI_AS_RAD_STATIC_CORRECTIONS = false;
 
-    // //to turn on the average corrections, enable the three lines below
-    // //note: these are designed to be used only if static corrections are also applied
-    // G4TPC::ENABLE_AVERAGE_CORRECTIONS = true;
-    // G4TPC::USE_PHI_AS_RAD_AVERAGE_CORRECTIONS = false;
-    // // to use a custom file instead of the database file:
-    // G4TPC::average_correction_filename = CDBInterface::instance()->getUrl("TPC_LAMINATION_FIT_CORRECTION");
+    //to turn on the average corrections, enable the three lines below
+    //note: these are designed to be used only if static corrections are also applied
+    G4TPC::ENABLE_AVERAGE_CORRECTIONS = true;
+    G4TPC::USE_PHI_AS_RAD_AVERAGE_CORRECTIONS = false;
+    // to use a custom file instead of the database file:
+    G4TPC::average_correction_filename = CDBInterface::instance()->getUrl("TPC_LAMINATION_FIT_CORRECTION");
     
     G4MAGNET::magfield_rescale = 1;
     TrackingInit();
