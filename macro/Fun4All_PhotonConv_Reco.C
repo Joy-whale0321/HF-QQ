d0e1bb52130dcd957ffff544dc97f6d81421e63f
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-10-08 16:42:01 +0800

    Update Fun4All_PhotonConv_Reco.C

--- macro/Fun4All_PhotonConv_Reco.C
+++ macro/Fun4All_PhotonConv_Reco.C
@@ -437,19 +437,19 @@ void KFPReco(std::string module_name = "KFPReco", std::string decaydescriptor = 
     kfparticle->setMaximumTrackchi2nDOF(100.); // JY
 
     // //Track-Calo matching
-    // kfparticle->set_emcal_radius_user(new_cemc_rad);
+    kfparticle->set_emcal_radius_user(new_cemc_rad);
     // //narrow window
     // kfparticle->set_dphi_cut_low(-0.02); //rad
     // kfparticle->set_dphi_cut_high(0.09); //rad
     // kfparticle->set_dz_cut_low(-4); //cm
     // kfparticle->set_dz_cut_high(4); //cm
     // //loose window
-    // // kfparticle->set_dphi_cut_low(-0.2); //rad
-    // // kfparticle->set_dphi_cut_high(0.2); //rad
-    // // kfparticle->set_dz_cut_low(-10); //cm
-    // // kfparticle->set_dz_cut_high(10); //cm
-    // kfparticle->set_emcal_e_low_cut(0.2); //GeV
-    // kfparticle->requireTrackEMCalMatch(true);
+    kfparticle->set_dphi_cut_low(-0.2); //rad
+    kfparticle->set_dphi_cut_high(0.2); //rad
+    kfparticle->set_dz_cut_low(-10); //cm
+    kfparticle->set_dz_cut_high(10); //cm
+    kfparticle->set_emcal_e_low_cut(0.2); //GeV
+    kfparticle->requireTrackEMCalMatch(true);
 
     //Vertex parameters
     kfparticle->setMaximumVertexchi2nDOF(30); // JY - from a vertex for pair electron
