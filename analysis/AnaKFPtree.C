386e2ac57af081415e950d22ec1906bf43a0830f
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2025-10-08 16:41:59 +0800

    Update AnaKFPtree.C

--- analysis/AnaKFPtree.C
+++ analysis/AnaKFPtree.C
@@ -18,6 +18,7 @@ void AnaKFPtree(
 {
     TFile outfile(outputname, "RECREATE");
     TH1D *h1_mass = new TH1D("h1_mass", "h1_mass; mass [GeV]; count", 100, 0, 1);
+    TH1D *h1_Rxy = new TH1D("h1_Rxy", "h1_Rxy; Rxy [cm]; count", 300, 0, 30);
     
     TFile *file_in = TFile::Open(filename);
     if (!file_in || file_in->IsZombie()) {
@@ -35,26 +36,50 @@ void AnaKFPtree(
     // std::vector <int> *clus_system = nullptr;
     // std::vector <double> *clus_X = nullptr;
     float mother_mass, mother_x, mother_y, mother_r;
+    float track_1_px, track_1_py, track_1_pz;
+    float track_2_px, track_2_py, track_2_pz;
 
     tree_data->SetBranchAddress("gamma_mass", & mother_mass);
     tree_data->SetBranchAddress("gamma_x", & mother_x);
     tree_data->SetBranchAddress("gamma_y", & mother_y);
 
-    mother_r = std::sqrt(mother_x*mother_x + mother_y*mother_y);
+    tree_data->SetBranchAddress("track_1_px", & track_1_px);
+    tree_data->SetBranchAddress("track_1_py", & track_1_py);
+    tree_data->SetBranchAddress("track_1_pz", & track_1_pz);
+
+    tree_data->SetBranchAddress("track_2_px", & track_2_px);
+    tree_data->SetBranchAddress("track_2_py", & track_2_py);
+    tree_data->SetBranchAddress("track_2_pz", & track_2_pz);
 
     Long64_t nentries = tree_data->GetEntries();
     for (Long64_t i=0; i<nentries; ++i) 
     {
         tree_data->GetEntry(i);
 
-        if(mother_r > 30) continue; // fiducial cut
-        if(mother_r < 1)
+        mother_r = std::sqrt(mother_x*mother_x + mother_y*mother_y);
 
-        h1_mass->Fill(mother_mass);
+        double mpion = 0.13957; // GeV
+        double p1 = sqrt(track_1_px*track_1_px + track_1_py*track_1_py + track_1_pz*track_1_pz);
+        double p2 = sqrt(track_2_px*track_2_px + track_2_py*track_2_py + track_2_pz*track_2_pz);
+        double Ee1 = sqrt(p1*p1 + mpion*mpion);
+        double Ee2 = sqrt(p2*p2 + mpion*mpion);
+        double m_recalc = sqrt(pow(Ee1+Ee2,2) - pow(track_1_px+track_2_px,2)
+                               - pow(track_1_py+track_2_py,2) - pow(track_1_pz+track_2_pz,2));
+
+        // h1_Rxy->Fill(mother_r);
+
+        // if(mother_r > 15) continue; 
+        if( ((mother_r > 2.5)&&(mother_r<4.5)) || ((mother_r>7)&&(mother_r<12)) )
+        // if( (mother_r < 0.5)||(mother_r>20) )
+        {
+            h1_Rxy->Fill(mother_r);
+            h1_mass->Fill(mother_mass);
+        }
     }
 
     outfile.cd();
     h1_mass->Write();
+    h1_Rxy->Write();
 
     std::cout<<"Ana Finished!"<<std::endl;
 }
